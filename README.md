8276f8de64e67df5b2cd2630b77c13dcaaf28269
rajapattanayak <pattanayak.raja@gmail.com>
2018-07-18 13:58:49 +0530

    Deployed to zeit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-Sample kickstarter dapp built over etherium smart contract
+Sample kickstarter dapp built over etherium smart contract.
+
+Access the client dapp frontend client from following location
+https://kickstart-czpdjcerqz.now.sh/
--- etherium/factory.js
+++ etherium/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from "./build/CampaignFactory.json";
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  process.env.CAMPAIGN_ADDRESS
+  "0x0078d25783F5d9Ab9A016F407d06b2a12457D108"
 );
 
 export default instance;
